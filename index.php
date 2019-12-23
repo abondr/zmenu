1ec2f769007fb0990c8bd93e18d3d0ee3f698f82
abondr <abondr@gmail.com>
2019-12-23 10:18:18 +0530

    master all files 23-12-2019

---
+++ index.php
@@ -0,0 +1,185 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Abon Duttaroy<abondr.job@gmail.com>">
+    <title>My Portfolio,Projects</title>
+
+<link href="libs/css/bootstrap.min.css" rel="stylesheet" >
+<link href="libs/css/style01.css" rel="stylesheet" >
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
+   <main role="main">
+
+  <section class="jumbotron">
+    <div class="container">
+      <h1 class='text-center'>Abon Duttaroy</h1>
+  <h2 class='text-center'>Projects and experience</h2>
+  <div class='row'>
+    <div class='col-md-9'>
+      <div class="lead text-muted">
+        <?php $y = date("Y") - 2013 + 1;?>
+        <b class='text-danger '>Freelancer</b> Web developer with <b class='bg-warning'><?=$y?> + </b> years of experience in 
+		    <ul>
+			    <li>PHP 
+            <ul>
+            <li>Codeigniter</li>
+            <li>Laravel</li>
+            <li>Yii</li>
+          </ul>
+        </li>
+			    <li>Mysql</li>
+			    <li>JavaScript
+          <ul>
+            <li>JQuery</li>
+          </ul>
+          </li>
+          <li>Python[selenium]</li>
+        </ul>
+	     </div>
+    </div>
+    <div class='col-md-3'>
+    <a href='CV- Abon Duttaroy.pdf'  data-toggle="tooltip" data-placement="top" title="Click to get CV">
+      <img src="libs/img/Abon_self_image.jpg" class="img-fluid" alt="Responsive image">
+    </a>
+    </div>
+    <div class='h2'>Scroll for projects<i class="fa fa-level-down" aria-hidden="true"></i></div>
+  </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-title">Codeigniter project 01</h5>
+              <p class="card-text">Basic project , modified later</p>
+              <a href="/codeigniter01/" class="btn btn-primary stretched-link">Go to project</a>
+            </div>
+          </div>
+        </div>
+          <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-title">Laravel project 01</h5>
+              <p class="card-text">Basic project , modified later</p>
+              <a href="http://larva01.zfreehost.xyz" class="btn btn-primary stretched-link">Go to project</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-title">Laravel project 02</h5>
+              <p class="card-text">Basic project , modified later</p>
+              <a href="http://larva02.zfreehost.xyz" class="btn btn-primary stretched-link">Go to project</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-title">Laravel project 03</h5>
+              <p class="card-text">Basic project , modified later</p>
+              <a href="http://larva03.zfreehost.xyz" class="btn btn-primary stretched-link">Go to project</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-title">Laravel project 04</h5>
+              <p class="card-text">Basic project , modified later</p>
+              <a href="http://larva04.zfreehost.xyz" class="btn btn-primary stretched-link">Go to project</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-title">Laravel project 05</h5>
+              <p class="card-text">Basic project , modified later</p>
+              <a href="http://larva05.zfreehost.xyz" class="btn btn-primary stretched-link">Go to project</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<footer class="text-muted">
+  <div class="container">
+    <p class="float-right">
+      <a href="#">Back to top</a>
+    </p>
+    <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+    <p>New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage</a> or read our <a href="/docs/4.4/getting-started/introduction/">getting started guide</a>.</p>
+  </div>
+</footer>
+<script src="libs/js/jquery.min.js"></script>
+<script src="libs/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
